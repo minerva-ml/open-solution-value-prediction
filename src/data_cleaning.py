a713aa982d82b3dc282d28932a1ed8375d1f0495
Jakub <czakon.jakub@gmail.com>
2018-06-26 12:18:37 +0200

    Dev drop fancyimpute (#29)
    * dropped fancyimpute
    
    * updated reqs
    
    * Update requirements.txt
    
    * Update requirements.txt

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,11 +1,8 @@
-import fancyimpute
 import numpy as np
 import pandas as pd
 import sklearn.feature_selection as fs
 from sklearn.externals import joblib
-from sklearn.preprocessing import Imputer
 from steppy.base import BaseTransformer
-from steppy.utils import get_logger
 
 
 class DropOneValueFrequent(BaseTransformer):
@@ -88,28 +85,3 @@ class DummiesMissing(BaseTransformer):
         X_is_missing = pd.DataFrame(missing_mask.astype(int), columns=missing_columns)
 
         return {'categorical_features': X_is_missing}
-
-
-class ImputeMissing(BaseTransformer):
-    def __init__(self, strategy='mean', missing_value=0):
-        self.strategy = strategy
-        self.missing_value = missing_value
-        self.imputer_knn = fancyimpute.KNN(3)
-        self.imputer_median = fancyimpute.SimpleFill(fill_method='median')
-
-    def transform(self, X, **kwargs):
-        missing_mask = np.where(X.values == self.missing_value, True, False)
-        missing_columns = ['{}_is_missing'.format(col) for col in X.columns]
-        X_is_missing = pd.DataFrame(missing_mask.astype(int), columns=missing_columns)
-
-        X_imputed = X.copy()
-        X_imputed[missing_mask] = np.nan
-        X_imputed = self.imputer_knn.complete(X_imputed)
-
-        missing_mask = np.where(X_imputed == 0, True, False)
-        X_imputed[missing_mask] = np.nan
-        X_imputed = self.imputer_median.complete(X_imputed)
-
-        X_imputed = pd.DataFrame(X_imputed, columns=X.columns)
-        return {'numerical_features': X_imputed,
-                'categorical_features': X_is_missing}
